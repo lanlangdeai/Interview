f43efbcba82c652e1f6c7e62d18650a5e43828a8
lanlang <liaotiam@126.com>
2019-04-07 13:08:55 +0800

    add some kinds of other language

--- PHP/sort.php
+++ PHP/sort.php
@@ -1,56 +1,56 @@
 <?php  
-
-// 常见排序
-
-
-// 1.冒泡排序
-function bubble_sort($data)
+/**
+ * 常见排序算法
+ * X-Wolf
+ * 2019-4-7
+ */
+class Sort
 {
-    $count = count($data);
-    if ($count <= 1) return $data;
+	// 冒泡排序
+	static function bubble_sort($data)
+	{
+	    $count = count($data);
+	    if ($count <= 1) return $data;
 
-    for($i = 0; $i < $count; $i++){
+	    for($i = 0; $i < $count; $i++){
+	        for($j = $i+1; $j < $count; $j++){
+	            if ($data[$i] > $data[$j]){
+	                $tmp = $data[$i];
+	                $data[$i] = $data[$j];
+	                $data[$j] = $tmp;
+	            }
+	        }
+	    }
 
-        for($j = $i+1; $j < $count; $j++){
-
-            if ($data[$i] > $data[$j]){
-
-                $tmp = $data[$i];
-
-                $data[$i] = $data[$j];
-
-                $data[$j] = $tmp;
-            }
-        }
-    }
-
-    return $data;
-}
-
-// 快速排序
-function quick_sort($data)
-{
-	$count = count($data);
-	if($count <= 1) return $data;
-
-	$standard = $data[0];
-	$leftData = $rightData = [];
-
-	for($i = 1; $i < $count; $i++){
-		if($data[$i] <= $standard){
-			$leftData[]  = $data[$i];
-		}else{
-			$rightData[] = $data[$i];
-		}
+	    return $data;
 	}
 
-	$leftData  = quick_sort($leftData);
-	$rightData = quick_sort($rightData);
+	// 快速排序
+	static function quick_sort($data)
+	{
+		$count = count($data);
+		if($count <= 1) return $data;
 
-	return array_merge($leftData,(array)$standard,$rightData);
+		$standard = $data[0];
+		$leftData = $rightData = [];
+
+		for($i = 1; $i < $count; $i++){
+			if($data[$i] <= $standard){
+				$leftData[]  = $data[$i];
+			}else{
+				$rightData[] = $data[$i];
+			}
+		}
+
+		$leftData  = self::quick_sort($leftData);
+		$rightData = self::quick_sort($rightData);
+
+		return array_merge($leftData,(array)$standard,$rightData);
+	}
+
 }
 
 $array = [3,435,2,12,4,3];
-// $ret = bubble_sort($array);
-$ret = quick_sort($array);
-var_dump($ret);
+// $ret = Sort::bubble_sort($array);
+$ret = Sort::quick_sort($array);
+print_r($ret);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Interview
 面试总结
+
+语言包括:PHP,Python,Go,Ruby,MySQL,Redis,MongoDB,Memcache
